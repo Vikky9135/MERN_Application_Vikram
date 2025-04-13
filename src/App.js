78703e585c4b411a50cd7a34bfc6cd457df64d17
Vikky9135 <vikramvikky2003@gmail.com>
2025-04-13 19:32:18 +0530

    Setup, Pages and Routes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Users from './users/pages/Users';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" /> 
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
